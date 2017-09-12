20c0b0b96ca6dd3a7fb45c69f2fd073c33dd91ad
仲澤 遼 <nakazawaryo@nakasawa-no-MacBook-Pro.local>
2017-09-12 15:36:38 +0800

    optgroup変更

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
         <p class="title-title">セブティー</p>
         <p class="title-subtitle">～留学の「イラナイ」を「ホシイ」に～</p>
       </div>
-      <div class="login">
+     <div class="login">
         <span>ログイン</span>
         <div class="login-text">
           <input type="text" name="user_name" placeholder="ユーザー名" class="input-text">
@@ -26,10 +26,10 @@
       <div class="search">
         <select name="genre" class="search1">
           <optgroup label="選択">
-          <option value="cloths">洋服</option>
-          <option value="appliances">電化製品</option>
-          <option　value="goods">雑貨品</option>
-          <option　value="ohters">その他</option>
+            <option value="cloths">洋服</option>
+            <option value="appliances">電化製品</option>
+            <option value="goods">雑貨品</option>
+            <option value="ohters">その他</option>
           </optgroup>
         </select>
         <input type="text" placeholder="キーワード" class="search2"><input type="submit" value="検索" class="search3">
