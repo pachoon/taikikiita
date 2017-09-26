68a031f75cd5d946afc8e4c3130706351e29719f
仲澤 遼 <nakazawaryo@nakasawa-no-MacBook-Pro.local>
2017-09-26 14:59:53 +0800

    test2

--- parts/footer.php
+++ parts/footer.php
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-  <footer>
-    <span class="footer-left-1">セブティー</span>
-    <span class="footer-right">セブティーの使い方</span>
-    <span class="footer-right">利用規約</span>
-    <span class="footer-right">プライバシーポリシー</span>
-    <br>
-    <span class="footer-left-2">～留学の「イラナイ」を「ホシイ」に～</span>
-    <span class="footer-right-1">©2017 TaikikiiTa all rigihts reserved</span>
-  </footer>
-=======
             <!-- Begin footer -->
             <footer class="text-off-white">
 
@@ -21,4 +10,3 @@
 
             </footer>
             <!-- End footer -->
->>>>>>> Zawa
--- parts/header.php
+++ parts/header.php
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-<header>
-  <div class="header-left">
-    <p class="header_title">
-      セブティー
-    </p>
-    <p class="header_subtitle">
-      ～留学の「イラナイ」を「ホシイ」に～
-    </p>
-  </div>
-  <div class="header-right">
-    <span>ホーム</span>
-    <span>マイページ</span>
-    <span>検索</span>
-    <span>チャット</span>
-    <span></span>
-  </div>
-</header>
-=======
             <!--========== BEGIN HEADER ==========-->
             <header id="header" class="header-main">
 
@@ -53,4 +34,3 @@
 
             </header>
             <!-- ========= END HEADER =========-->
->>>>>>> Zawa
